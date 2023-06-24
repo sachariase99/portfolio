54f321a7e728582b33e46b861a61c89eac0972f7
sachariase99 <sachariase99@hotmail.com>
2023-06-24 18:22:03 +0200

    Summary

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='bg-darkpurple flex justify-center mt-8 p-8'>
-      <p className='text-whiteopacity text-lg'>copyright © Sagar Bhusal . 2022 </p>
+      <p className='text-whiteopacity text-lg'>copyright © <span className='text-purple'>J</span>annick<span className='text-purple'> S</span>achariasen . 2022 </p>
     </div>
   )
 }
--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -10,12 +10,12 @@ const Header = () => {
                 <div>
                     <Navbar />
                 </div>
-                <div className='grid md:grid-cols-3 items-center pt-[9rem] pb-8'>
+                <div className='grid md:grid-cols-3 items-center pt-[9rem] pb-4'>
                     <h1 className='text-white font-extrabold text-[2.4rem] leading-tight pb-4 md:pb-0 text-center md:text-left'><span className='text-purple'>J</span>annick<span className='text-purple'> S</span>achariasen</h1>
                     <div className='bg-purple rounded-full w-[70%] m-auto md:w-[100%]'>
                         <img className='opacity-90 w-[100%]' src={Avatar} alt="Avatar" />
                     </div>
-                    <div className='text-purple opacity-50 flex justify-center md:justify-end items-center pt-4 md:pt-4 absolute bottom-12 right-8'>
+                    <div className='text-purple opacity-50 flex justify-center md:justify-end items-center pt-4 md:absolute md:bottom-12 md:right-8'>
                         <a href="#contact" className='z-0 flex items-center cursor-pointer'>
                             <p className='font-bold text-[2rem] bg-transparent'><PiPaperPlaneTiltLight /></p><p className='font-bold text-[1.4vw]'>Let's Talk</p>
                         </a>
