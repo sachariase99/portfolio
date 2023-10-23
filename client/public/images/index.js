d5d0f506af1c3b6933708083049f568600f7c5b0
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2023-10-23 14:57:35 +0200

    s

---
+++ client/public/images/index.js
@@ -0,0 +1,41 @@
+import Avatar from './Jannick.png'
+import JannickAvatar from './JannickAvatar.png'
+import Jannick from './Jannick.jpg'
+import logo from './Logo.png'
+import code from './CodeIcon.svg'
+import design from './DesignIcon.svg'
+import project from './ProjectsIcon.svg'
+import tailwind from './TailwindCSS.png'
+import nodejs from './Nodejs.png'
+import github from './Github.png'
+import figma from './Figma.png'
+import html from './HTML5.png'
+import react from './React.png'
+import javascript from './Javascript.png'
+import cucode from './cucode.png'
+import tshop from './T-Shop.jpg'
+import spicy from './spicy.jpg'
+import globalGoals from './globalGoals.png'
+import gpt from './GPT-3.jpg'
+
+export {
+    Avatar,
+    JannickAvatar,
+    Jannick,
+    logo,
+    code,
+    design,
+    project,
+    tailwind,
+    nodejs,
+    github,
+    figma,
+    html,
+    react,
+    javascript,
+    cucode,
+    tshop,
+    spicy,
+    globalGoals,
+    gpt,
+}
--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,37 +1,42 @@
-const cacheName = 'cache-v1';
+const cacheName = 'cache-v2';
 
-// Installation Event: Caching static assets
 self.addEventListener('install', event => {
     console.log('Installing');
     event.waitUntil(
-        // Open a cache with the specified cacheName
         caches.open(cacheName).then(cache => {
             console.log('Caching');
-            // Add specified static assets to the cache
             return cache.addAll([
                 '/',
                 '/index.html',
-                '/static/css/main.css',
-                '/static/js/main.js',
-                '/images/logo.png',
+                '/images/Avatar.png',
+                '/static/js/bundle.js',
+                '/static/media/JannickAvatar.c6b01ce117b441ca64f4.png',
+                '/static/media/Jannick.28e1053f8db6882505a1.jpg',
+                '/static/media/CodeIcon.575af008878ffae52c142b0e714f3111.svg',
+                '/static/media/Logo.199def0cd275e38f1b42.png',
+                '/static/media/DesignIcon.f57e5e9fab71dab089f2367fc9983ab7.svg',
+                '/static/media/ProjectsIcon.0914bb1496e5afc107cf7a8c73f5e59b.svg',
+                '/static/media/cucode.c82c5734549efcdf8853.png',
+                '/static/media/T-Shop.3072f3b1fc4cdcbd8b54.jpg',
+                '/static/media/spicy.2b9d3d264de3ee864d21.jpg',
+                '/static/media/globalGoals.0aec8e12fbcc916a3fce.png',
+                '/static/media/GPT-3.26ca2970fdbcb2a86e12.jpg',
+                '/manifest.json',
+                '/Logo.png',
+                '/logo/android-launchericon-144-144.png',
             ]);
         })
     );
 });
 
-// Activation Event: Remove old caches
 self.addEventListener('activate', event => {
     console.log('Activating');
     event.waitUntil(
-        // Get a list of all cache names
         caches.keys().then(cacheNames => {
             return Promise.all(
-                // Iterate through each cache name
                 cacheNames.map(existingCacheName => {
-                    // Check if the cache name is not the current cacheName
                     if (existingCacheName !== cacheName) {
                         console.log('Deleting old cache', existingCacheName);
-                        // Delete the old cache
                         return caches.delete(existingCacheName);
                     }
                 })
@@ -40,13 +45,10 @@ self.addEventListener('activate', event => {
     );
 });
 
-// Fetch Event: Serve cached assets or fetch from the network
 self.addEventListener('fetch', event => {
     console.log('Service Worker: Fetching', event.request.url);
     event.respondWith(
-        // Try to match the request with a cached response
         caches.match(event.request).then(response => {
-            // Return the cached response, or fetch from the network if not found in the cache
             return response || fetch(event.request);
         })
     );
