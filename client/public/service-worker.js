07e9d50ab3da74011c31fbf32a1a5df67bd387ac
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2023-10-24 08:49:42 +0200

    s

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -7,31 +7,36 @@ self.addEventListener('install', event => {
 
     // Wait until the cache is opened and assets are added
     event.waitUntil(
-        caches.open(cacheName).then(cache => {
-            // Log that we are caching assets
+        (async () => {
+            const cache = await caches.open(cacheName);
             console.log('Caching');
-            return cache.addAll([
-                // Cache the following assets
-                '/',
-                '/index.html',
-                '/images/Avatar.png',
-                '/static/js/bundle.js',
-                '/static/media/JannickAvatar.c6b01ce117b441ca64f4.png',
-                '/static/media/Jannick.28e1053f8db6882505a1.jpg',
-                '/static/media/CodeIcon.575af008878ffae52c142b0e714f3111.svg',
-                '/static/media/Logo.199def0cd275e38f1b42.png',
-                '/static/media/DesignIcon.f57e5e9fab71dab089f2367fc9983ab7.svg',
-                '/static/media/ProjectsIcon.0914bb1496e5afc107cf7a8c73f5e59b.svg',
-                '/static/media/cucode.c82c5734549efcdf8853.png',
-                '/static/media/T-Shop.3072f3b1fc4cdcbd8b54.jpg',
-                '/static/media/spicy.2b9d3d264de3ee864d21.jpg',
-                '/static/media/globalGoals.0aec8e12fbcc916a3fce.png',
-                '/static/media/GPT-3.26ca2970fdbcb2a86e12.jpg',
-                '/manifest.json',
-                '/Logo.png',
-                '/logo/android-launchericon-144-144.png'
-            ]);
-        })
+            try {
+                const resources = [
+                    // Cache the following assets
+                    '/',
+                    '/index.html',
+                    '/images/Avatar.png',
+                    '/static/js/bundle.js',
+                    '/static/media/JannickAvatar.c6b01ce117b441ca64f4.png',
+                    '/static/media/Jannick.28e1053f8db6882505a1.jpg',
+                    '/static/media/CodeIcon.575af008878ffae52c142b0e714f3111.svg',
+                    '/static/media/Logo.199def0cd275e38f1b42.png',
+                    '/static/media/DesignIcon.f57e5e9fab71dab089f2367fc9983ab7.svg',
+                    '/static/media/ProjectsIcon.0914bb1496e5afc107cf7a8c73f5e59b.svg',
+                    '/static/media/cucode.c82c5734549efcdf8853.png',
+                    '/static/media/T-Shop.3072f3b1fc4cdcbd8b54.jpg',
+                    '/static/media/spicy.2b9d3d264de3ee864d21.jpg',
+                    '/static/media/globalGoals.0aec8e12fbcc916a3fce.png',
+                    '/static/media/GPT-3.26ca2970fdbcb2a86e12.jpg',
+                    '/manifest.json',
+                    '/Logo.png',
+                    '/logo/android-launchericon-144-144.png'
+                ];
+                await cache.addAll(resources);
+            } catch (error) {
+                console.error('Failed to cache one or more resources:', error);
+            }
+        })()
     );
 });
 
@@ -52,7 +57,10 @@ self.addEventListener('activate', event => {
                         return caches.delete(existingCacheName);
                     }
                 })
-            );
+            ).then(() => {
+                // After deleting old caches, you can call the limitCacheSize function
+                limitCacheSize(cacheName, 30);  // Limit the cache size to 30 items
+            });
         })
     );
 });
@@ -66,17 +74,26 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request).then(response => {
             // Check if the requested resource is in the cache
-            return response || fetch(event.request).then(networkResponse => {
+            if (response) {
+                return response;
+            }
+
+            // If the requested resource is not in the cache, return a custom fallback page
+            return fetch(event.request).then(networkResponse => {
                 // If not in the cache, add it to the cache and limit cache size
                 return caches.open(cacheName).then(cache => {
                     cache.put(event.request, networkResponse.clone());
                     limitCacheSize(cacheName, 30);  // Limit the cache size to 30 items
                     return networkResponse;
                 });
+            }).catch(error => {
+                // If the network request fails, return a custom fallback page
+                return caches.match('/fallback/fallback.html');
             });
         })
     );
 });
+
 
 // Function to limit the cache size
 const limitCacheSize = (cacheName, numberOfAllowedFiles) => {
@@ -88,4 +105,4 @@ const limitCacheSize = (cacheName, numberOfAllowedFiles) => {
             }
         });
     });
-};
+}
