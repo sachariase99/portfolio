2c5b46198d95a839ed8174d01aab953f54b52221
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2023-10-24 10:18:04 +0200

    Update service-worker.js

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,4 +1,4 @@
-const cacheName = 'cache-v2'; // Cache version
+const cacheName = 'cache-v3'; // Cache version
 
 // Event listener for the 'install' event
 self.addEventListener('install', event => {
