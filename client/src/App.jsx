2e781c5eff75495f4fd07935e54c438efc174132
sachariase99 <126781566+sachariase99@users.noreply.github.com>
2023-10-27 08:59:33 +0200

    s

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,23 @@
 import './App.css'
 import { Header, Card, About, Technologies, Portfolio, Contact, Footer, LandingPage } from './components/index'
 import { useEffect } from 'react'
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCYbPKSjLEyBYnxOFSVwtbyQJwdwqVWT0Q",
+  authDomain: "portfolio-pwa-ca9fc.firebaseapp.com",
+  projectId: "portfolio-pwa-ca9fc",
+  storageBucket: "portfolio-pwa-ca9fc.appspot.com",
+  messagingSenderId: "1038457658585",
+  appId: "1:1038457658585:web:916bb651070cf353fcb2c8",
+  measurementId: "G-MRWYXL37YM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 
 function App() {
   useEffect(() => {
--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,20 +1,37 @@
-import React from 'react';
-import { logo } from '../images';
-import BurgerMenu from './burgermenu';
+import React, { useState, useEffect } from 'react';
+import { getStorage, ref, getDownloadURL } from 'firebase/storage';
 
 const Navbar = () => {
-    return (
-        <nav className='fixed top-8 left-[5vw] right-[5vw] rounded-xl z-50 bg-darkpurple'>
-            <div className='flex justify-between items-center p-3'>
-                <a href="#header">
-                    <img className='max-w-[50px]' src={logo} alt='Logo' />
-                </a>
-                <div className='z-10'>
-                    <BurgerMenu />
-                </div>
-            </div>
-        </nav>
-    );
+  const [logoURL, setLogoURL] = useState(null);
+
+  useEffect(() => {
+    const storage = getStorage();
+    const storageRef = ref(storage);
+    const logoRef = ref(storage, 'images/Logo.png');
+
+    getDownloadURL(logoRef)
+      .then((url) => {
+        setLogoURL(url);
+      })
+      .catch((error) => {
+        console.error('Error getting download URL for the logo:', error);
+      });
+  }, []);
+
+  return (
+    <nav className='fixed top-8 left-[5vw] right-[5vw] rounded-xl z-50 bg-darkpurple'>
+      <div className='flex justify-between items-center p-3'>
+        <a href="#header">
+          {logoURL && (
+            <img className='max-w-[50px]' src={logoURL} alt='Logo' />
+          )}
+        </a>
+        <div className='z-10'>
+          {/* Your BurgerMenu component */}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
